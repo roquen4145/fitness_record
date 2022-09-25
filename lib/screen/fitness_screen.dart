2f4171e1553828771d45725e983ff5a2f2799c3c
roquen4145 <roquen4145@gmail.com>
2022-09-25 22:18:32 +0900

    add search field

--- lib/screen/fitness_screen.dart
+++ lib/screen/fitness_screen.dart
@@ -9,6 +9,18 @@ class FitnessScreen extends StatefulWidget {
 }
 
 class _FitnessScreenState extends State<FitnessScreen> {
+  final TextEditingController _filter = TextEditingController();
+  FocusNode focusNode = FocusNode();
+  String _fitnessSearchText = "";
+
+  _FitnessScreenState() {
+    _filter.addListener(() {
+      setState(() {
+        _fitnessSearchText = _filter.text;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +36,49 @@ class _FitnessScreenState extends State<FitnessScreen> {
           Text("Fitness List", style: TextStyle(fontSize: 18)),
           Padding(
             padding: EdgeInsets.all(20),
+          ),
+          Container(
+            // flex: 6,
+            child: TextField(
+              focusNode: focusNode,
+              style: TextStyle(fontSize: 15),
+              autofocus: true,
+              controller: _filter,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Color.fromRGBO(157, 172, 220, 100),
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.white60,
+                  size: 20,
+                ),
+                suffixIcon: focusNode.hasFocus
+                    ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _filter.clear();
+                      _fitnessSearchText = "";
+                    });
+                  },
+                  icon: Icon(
+                    Icons.cancel,
+                    size: 20,
+                  ),
+                )
+                    : Container(),
+                hintText: '검색',
+                labelStyle: TextStyle(color: Colors.white),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+              ),
+            ),
           ),
           // replace with firebase
           Table(
