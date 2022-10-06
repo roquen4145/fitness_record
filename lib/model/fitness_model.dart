223121c15c39a2d272d7fde48b044e854c91a9ba
roquen4145 <roquen4145@gmail.com>
2022-10-06 22:02:31 +0900

    redesign database format

--- lib/model/fitness_model.dart
+++ lib/model/fitness_model.dart
@@ -2,14 +2,51 @@ class Fitness {
   final int id;
   final String name;
   final String bodypart;
+  final String uid;
+  final String memo;
+  final double rm;
 
-  Fitness({required this.id,required this.name,required this.bodypart});
+  Fitness(
+      {required this.id,
+      required this.name,
+      required this.bodypart,
+      required this.uid,
+      required this.memo,
+      required this.rm});
 
   Map<String, dynamic> toMap() {
     return {
-      'id' : id,
-      'name' : name,
-      'bodypart' : bodypart,
+      'id': id,
+      'name': name,
+      'bodypart': bodypart,
+      'uid': uid,
+      'memo': memo,
+      'rm': rm,
+    };
+  }
+}
+
+class FitnessRecord {
+  final String uid;
+  final int fid;
+  final int setNum;
+  final double weight;
+  final DateTime time;
+
+  FitnessRecord(
+      {required this.uid,
+      required this.fid,
+      required this.setNum,
+      required this.weight,
+      required this.time});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'fid': fid,
+      'setNum': setNum,
+      'weight': weight,
+      'time': time,
     };
   }
 }
