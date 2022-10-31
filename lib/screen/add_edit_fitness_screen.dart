09ca7a41ad551b11a85ecf058875dc4fde99a1bb
roquen4145 <roquen4145@gmail.com>
2022-10-31 21:17:39 +0900

    update addeditscreen

--- lib/screen/add_edit_fitness_screen.dart
+++ lib/screen/add_edit_fitness_screen.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
 
+import '../model/fitness_model.dart';
+
 class AddEditFitnessScreen extends StatefulWidget {
-  const AddEditFitnessScreen({Key? key}) : super(key: key);
+  final Fitness? fitness;
+
+  const AddEditFitnessScreen({Key? key, this.fitness}) : super(key: key);
 
   @override
   State<AddEditFitnessScreen> createState() => _AddEditFitnessScreenState();
 }
 
 class _AddEditFitnessScreenState extends State<AddEditFitnessScreen> {
+  final _formKey = GlobalKey<FormState>();
+  late String name;
+  late String bodypart;
+
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  void initState() {
+    super.initState();
+
+    name = widget.fitness?.name ?? '';
+    bodypart = widget.fitness?.bodypart ?? '';
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          actions: [buildButton()],
+        ),
+        body: Form(key: _formKey, child: Container()),
+      );
+
+  Widget buildButton() {
+    final isFormValid = name.isNotEmpty && bodypart.isNotEmpty;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          onPrimary: Colors.white,
+          primary: isFormValid ? null : Colors.grey.shade700,
+        ),
+        onPressed: () {},
+        child: Text('Save'),
+      ),
+    );
   }
 }
